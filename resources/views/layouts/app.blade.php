2a7ac2b780b23bb9712793b7f620bcb73e95234b
lucassrondon <lucas.rondon@hotmail.com>
2023-08-27 01:27:32 -0300

    parte da listagem de documentos pronta

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,10 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
+
+        <!-- Font awesome script -->
+        <script src="https://kit.fontawesome.com/0ca14f61f4.js" crossorigin="anonymous"></script>
+
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
         <!-- Styles -->
--- resources/views/livewire/document-list.blade.php
+++ resources/views/livewire/document-list.blade.php
@@ -6,28 +6,32 @@
         </h2>
     </x-slot>
 
-    <div>
-        <ul>
+    <!-- Upload success and failure message divs -->
+    @if (session()->has('successMessage'))
+        <div class="text-sm text-green-600 mt-2">{{ session('successMessage') }}</div>
+    @endif
+    @if (session()->has('failMessage'))
+        <div class="text-sm text-red-600 mt-2">{{ session('failMessage') }}</div>
+    @endif
+
+    <!-- Div to list the documents as links -->
+    <div class="grid grid-cols-1 gap-4 md:grid-cols-3 p-6">
+            <!-- Listing the documents; each document is a link to open it with details -->
             @foreach ($documentsList as $document)
-                <li>Title: {{ $document->title }}</li>
-                <li>Description: {{ $document->description }}</li>
-                <li>Type: {{ $document->mime_type }}</li>
-
-                <a href="{{ route('document-update', ['document' => $document->id]) }}">Update</a>
-                @if ($documentToDelete != $document->id)
-                    <button wire:click="setDocumentToDelete( {{ $document->id }} )">Delete</button>
-                @else
-                    <button wire:click="confirmDocumentDelete">Confirm Delete</button>
-                    <button wire:click="cancelDocumentDelete">Cancel</button>
-                @endif
+            <a href="" class="flex flex-col gap-4 scale-100 p-6 bg-white from-gray-700/50 via-transparent rounded-lg shadow-2xl shadow-gray-500/20 motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-indigo-500">
+                <!-- Icon of the document type and the creation date of the document -->
+                <div class="flex justify-between">
+                    @if ($document->mime_type == 'application/pdf')
+                        <i class="fa-solid fa-file-pdf fa-2x"></i>
+                    @endif
+                    <p class=" text-gray-500 text-sm leading-relaxed">{{ $document->created_at }}</p>
+                </div>
+                
+                <!-- Title and description of the document -->
+                <h2 class="p-2 truncate font-semibold text-x text-black uppercase tracking-widest bg-gray-500 border border-transparent rounded-md"> {{ $document->title }}</h2>
+                <p class="truncate text-gray-500 text-sm leading-relaxed">{{ $document->description }}</p>
+            </a>
             @endforeach
-        </ul>
-
-        @if (session()->has('successMessage'))
-            <div class="">{{ session('successMessage') }}</div>
-        @endif
-        @if (session()->has('failMessage'))
-            <div class="">{{ session('failMessage') }}</div>
-        @endif
     </div>
+
 </div>
