38a7b0cbec0e3eb7cf0a570c1fa782ad4c46ece3
lucassrondon <lucas.rondon@hotmail.com>
2023-08-31 23:40:23 -0300

    no uploads message

--- resources/views/livewire/document-list.blade.php
+++ resources/views/livewire/document-list.blade.php
@@ -9,31 +9,37 @@
     <!-- Upload success and failure message divs -->
     <x-success-or-fail-message/>
 
-    <!-- Div to list the documents as links -->
-    <div class=" grid grid-cols-1 gap-4 md:grid-cols-3 p-6">
-            <!-- Listing the documents; each document is a link to open it with details -->
-            @foreach ($documentsList as $document)
-            <a href="{{ route('document-details', $document->id) }}" class="flex flex-col gap-4 scale-100 p-6 bg-white from-gray-700/50 via-transparent rounded-lg shadow-2xl shadow-gray-500/20 motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-indigo-500">
-                <!-- Icon of the document type and the creation date of the document -->
-                <div class="flex justify-between">
-                    <img src="{{ asset('icons/'.$document->getExtension().'_icon.png') }}" alt="Logo" class="w-12">
-                    <x-description-style> 
-                        {{ $document->created_at }} 
+    @if (count($documentsList) == 0)
+        <!-- Image with message when user has no uploads -->
+        <div class="flex justify-center items-center">
+            <img src="{{ asset('images/no_uploads_logo.png') }}" alt="">
+        </div>
+    @else
+        <!-- Div to list the documents as links -->
+        <div class=" grid grid-cols-1 gap-4 md:grid-cols-3 p-6">
+                <!-- Listing the documents; each document is a link to open it with details -->
+                @foreach ($documentsList as $document)
+                <a href="{{ route('document-details', $document->id) }}" class="flex flex-col gap-4 scale-100 p-6 bg-white from-gray-700/50 via-transparent rounded-lg shadow-2xl shadow-gray-500/20 motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-indigo-500">
+                    <!-- Icon of the document type and the creation date of the document -->
+                    <div class="flex justify-between">
+                        <img src="{{ asset('icons/'.$document->getExtension().'_icon.png') }}" alt="Logo" class="w-12">
+                        <x-description-style> 
+                            {{ $document->created_at }} 
+                        </x-description-style>
+                    </div>
+                    
+                    <!-- Title and description of the document -->
+                    <x-doc-details-background>
+                        <x-upper-title overflowCase="truncate"> 
+                            {{ $document->title }} 
+                        </x-upper-title>
+                    </x-doc-details-background>
+                    
+                    <x-description-style class="truncate">
+                        {{ $document->description }}
                     </x-description-style>
-                </div>
-                
-                <!-- Title and description of the document -->
-                <x-doc-details-background>
-                    <x-upper-title overflowCase="truncate"> 
-                        {{ $document->title }} 
-                    </x-upper-title>
-                </x-doc-details-background>
-                
-                <x-description-style class="truncate">
-                    {{ $document->description }}
-                </x-description-style>
-            </a>
-            @endforeach
-    </div>
-
+                </a>
+                @endforeach
+        </div>
+    @endif
 </div>
